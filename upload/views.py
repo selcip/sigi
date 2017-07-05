43a5b4233d413a830d67e5a41833990aff94933d
Luiz Eduardo C <eduselcip@gmail.com>
2017-07-05 02:23:02 -0300

    login

--- upload/views.py
+++ upload/views.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("fala memo")
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
 
